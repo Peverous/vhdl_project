973f450bc724bb190af04b288d56121cd937217f
Chris Andriakopoulos (Peverous) <xristos4870@gmail.com>
2017-03-15 16:14:36 +0200

    Update add_generator.vhd

--- add_generator.vhd
+++ add_generator.vhd
@@ -5,13 +5,13 @@ use ieee.std_logic_unsigned.all;
 use work.constants.all;
 
 entity add_generator is
-  port (clk     : in std_logic;
-        rst     : in std_logic;
-        enable_rom : in std_logic;
+  port (clk     	: in std_logic;
+        rst     	: in std_logic;
+        enable_rom 	: in std_logic;
         enable_ram  : in std_logic;
-        addr1    : out std_logic_vector(addr_range1-1 downto 0);
-        addr2    : out std_logic_vector(addr_range1-1 downto 0);
-		    addr3	: out std_logic_vector(addr_range2-1 downto 0));
+        addr1    	: out std_logic_vector(addr_range1-1 downto 0);
+        addr2    	: out std_logic_vector(addr_range1-1 downto 0);
+		addr3		: out std_logic_vector(addr_range2-1 downto 0));
   end add_generator;
   
 architecture behavior of add_generator is
@@ -20,9 +20,9 @@ signal cnt      : std_logic_vector(addr_range1-2 downto 0);
 signal temp     : std_logic_vector(addr_range2-1 downto 0);
 begin
   
-U_rom_addr:  process (clk)
-    begin
-      if (clk'event and clk='1') then
+U_rom_addr:	process (clk)
+begin
+	if (clk'event and clk='1') then
         if (rst='1') then
           cnt<=(others=>'0');
         elsif (enable_rom='1') then
@@ -49,10 +49,6 @@ U_ram_addr : process (clk)
           temp<=temp;
         end if;
       end if;
-  end process U_ram_addr;
-  
-  
+  end process U_ram_addr;  
 addr3<=temp;
 end behavior;
-
-  
